5b3e4c8f661d3ce227b7ad638044d7e047e34c16
Averrous Saloom <averous009@gmail.com>
2025-01-01 11:33:12 +0700

    feat: working chat with our stream

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,7 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { NextResponse } from "next/server";
 import { StringOutputParser } from "@langchain/core/output_parsers";
-export async function POST(req: Request) {
+export async function POST(req: Request, res: NextResponse) {
   const { messages } = await req.json();
   const model = new ChatOpenAI({
     modelName: "gpt-4o",
@@ -10,12 +10,41 @@ export async function POST(req: Request) {
   const chain = model.pipe(new StringOutputParser());
   const response = await chain.stream(messages);
 
-  return new Response(response, {
+
+  const encoder = new TextEncoder();
+  const stream = new ReadableStream({
+    async start(controller) {
+      for await (const chunk of response) {
+        const escapedChunk = chunk.replace(/\n/g, '\\n');
+        controller.enqueue(encoder.encode(`0:"${escapedChunk}"\n`));
+      }
+      controller.close();
+    },
+  });
+
+  return new NextResponse(stream, {
     headers: {
-      'Content-Type': '"text/plain; charset=utf-8"',
+      'Content-Type': 'text/event-stream',
       'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive',
-      'Transfer-Encoding': 'chunked'
-    }
+      Connection: 'keep-alive',
+    },
   });
 }
+
+// import { openai } from '@ai-sdk/openai';
+// import { streamText } from 'ai';
+
+// // Allow streaming responses up to 30 seconds
+// export const maxDuration = 30;
+
+// export async function POST(req: Request) {
+//   const { messages } = await req.json();
+
+//   const result = streamText({
+//     model: openai('gpt-4-turbo'),
+//     system: 'You are a helpful assistant.',
+//     messages,
+//   });
+
+//   return result.toDataStreamResponse();
+// }
